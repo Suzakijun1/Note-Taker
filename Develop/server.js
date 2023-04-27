79ea1fcb65162d2cdc4439af6bb29c0a272c9366
suzakijun1 <dyong1997@gmail.com>
2023-04-27 12:20:46 -0400

    server.js info

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const { clog } = require("./middleware/clog");
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+//import custom middleware, cLog
+app.use(clog);
+
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.use(express.static("public"));
+
+// GET route for homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET route for notes
+app.use("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
