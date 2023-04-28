001cbeac5a99728e0515129ed32e27420cf5e389
suzakijun1 <dyong1997@gmail.com>
2023-04-28 17:17:49 -0400

    working apiroute

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,16 +1,39 @@
-const api = require("express").Router();
-const { readFromFile, readAndAppend } = require("../fsUtils");
+const router = require("express").Router();
+// const { readFromFile, readAndAppend } = require("../fsUtils");
 path = require("path");
+
+const { json } = require("express");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 //GET route for retrieving all the api
 
-api.get("/notes", (req, res) => {
+router.get("/notes", (req, res) => {
   if (error) {
     return console.log(error);
   }
   res.json(JSON.parse(notes));
 });
 
-//POST Route
+//POST Route for note
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+  console.log("New Note:" + JSON.stringify(newNote));
 
-module.exports = api;
+  newNote.id = uuidv4();
+
+  let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+  data.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(data));
+  console.log("Successfully added new note!");
+  res.json(data);
+
+  // readAndAppend(JSON.stringify(notes), (err) => {
+  //   if (err) throw err;
+  //   res.json(db);
+  //   console.log("Success");
+});
+
+module.exports = router;
